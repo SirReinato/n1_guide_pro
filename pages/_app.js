ce23931b972a4c853a4b963a03e5343e8941a4c6
SirReinato <renatofranca2881@gmail.com>
2026-01-25 10:18:48 -0300

    criando context de busca

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,18 @@ import GlobalStyle from "../src/theme/GlobalStyle";
 import { ConteinerGeral } from "../src/theme/theme";
 import Header from "../src/components/patterns/Header";
 import Footer from "../src/components/patterns/Footer";
+import { BuscaProvider } from "../src/context/BuscaContext";
 
 function MyApp({ Component, pageProps }) {
     return (
-        <>
+        <BuscaProvider>
             <GlobalStyle />
             <ConteinerGeral>
                 <Header />
                 <Component {...pageProps} />
                 <Footer />
             </ConteinerGeral>
-        </>
+        </BuscaProvider>
     );
 }
 
